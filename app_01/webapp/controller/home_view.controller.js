ceab019faecd38728bc9b036994a4052dc432cbc
MrDominicSamo <dominic.samo@res.co.sz>
2026-08-17 09:27:18 +0200

    Form input

--- app_01/webapp/controller/home_view.controller.js
+++ app_01/webapp/controller/home_view.controller.js
@@ -1,10 +1,13 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
 
-    return Controller.extend("app01.controller.home_view", {
-        onInit() {
-        }
-    });
+  return Controller.extend("app01.controller.home_view", {
+    onInit() {},
+    alertButton: function () {
+      let username = this.getView().byId("_username").getValue();
+      let password = this.getView().byId("_password").getValue();
+
+      alert(`Username: ${username}\n Password: ${password}`);
+    },
+  });
 });
