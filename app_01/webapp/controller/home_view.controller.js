c2b6db29a171dd5568525f2fabe299c49e85855d
Dominic Samo <dominic.samo@res.co.sz>
2026-08-17 09:09:56 +0000

    Reading data from External System

--- app_01/webapp/controller/home_view.controller.js
+++ app_01/webapp/controller/home_view.controller.js
@@ -9,14 +9,15 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
         .attachPatternMatched(this._onObjectMatched, this);
 
       // Initialize the model
-      var oModel = new sap.ui.model.json.JSONModel();
-      this.getView().setModel(oModel);
+      var oUserModel = new sap.ui.model.json.JSONModel();
+      this.getView().setModel(oUserModel, "oUserModel");
     },
     _onObjectMatched: function (oEvent) {
       var user_name = oEvent.getParameter("arguments").name;
       var pass_word = oEvent.getParameter("arguments").pass;
-      this.getView().getModel().setProperty("/username", user_name);
-      this.getView().getModel().setProperty("/password", pass_word);
+      var oUserModel = this.getView().getModel("oUserModel");
+      oUserModel.setProperty("/username", user_name);
+      oUserModel.setProperty("/password", pass_word);
     },
     loginButton: function () {
       let username = this.getView().byId("_username").getValue();
